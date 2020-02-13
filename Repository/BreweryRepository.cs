7154f95acbe4889ae95f2533fd35a05b32622391
AV25242 <arunv.vijay@gmail.com>
2020-02-12 22:35:40 -0600

    Add files via upload

---
+++ Repository/BreweryRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase.Core;
+using Couchbase.Configuration.Client;
+using Couchbase.Authentication;
+using Couchbase.Api.Models;
+using Couchbase;
+
+
+namespace Couchbase.Api.Repository
+{
+    public class BreweryRepository
+    {
+        private readonly IBucket _bucket;
+
+        public BreweryRepository(ICouchbaserServerConfiguration couchbaseConfig)
+        {
+            ClusterHelper.Initialize(new ClientConfiguration
+            {
+                Servers = new List<Uri> { new Uri(couchbaseConfig.Host) }
+            }, new PasswordAuthenticator(couchbaseConfig.Username, couchbaseConfig.Password));
+
+            _bucket = ClusterHelper.GetBucket(couchbaseConfig.Bucket);
+        }
+
+        #region Read(s) 
+
+
+        public Brewery GetBrewery(string id)
+        {
+            var result = _bucket.Get<Brewery>(id);
+            return result.Value;
+        }      
+
+        #endregion
+
+        #region Create(s)
+        public Brewery AddBrewery(string id, Brewery brewery)
+        {
+            var result = _bucket.Insert(id, brewery);
+
+            if (result.Success)
+            {
+                return _bucket.Get<Brewery>(result.Id).Value;
+            }
+
+            return null;
+        }       
+
+        #endregion
+
+        #region Update(s)
+        public Brewery EditBrewery(string id, Brewery brewery)
+        {
+            var result = _bucket.Upsert(id, brewery);
+
+            if (result.Success)
+            {
+                return _bucket.Get<Brewery>(result.Id).Value;
+            }
+
+            return null;
+        }
+
+      
+
+        #endregion
+
+        #region Delete(s)
+        public bool DeleteBrewery(string id)
+        {
+            var result = _bucket.Remove(id);
+
+            return result.Success;
+        }   
+
+        #endregion
+    }
+}
---
+++ Services/BreweryService.cs
@@ -0,0 +1,71 @@
+﻿using Couchbase.Api.Models;
+using Couchbase.Api.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Couchbase.Api.Services
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BreweryService : Controller
+    {
+        private readonly BreweryRepository _repository;
+
+        public BreweryService(BreweryRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        [Route("api/GetBrewery/{id}")]
+        public IActionResult GetBrewery(string id)
+        {
+            var result = _repository.GetBrewery(id);
+
+            return Ok(result);
+        }     
+        [HttpPost]
+        [Route("api/AddBrewery")]
+        public IActionResult AddBrewery([FromBody] Brewery brewery)
+        {
+            if (string.IsNullOrEmpty(brewery.Id))
+            {
+                return Ok("No Id for brewery.  No action taken.");
+            }
+
+            var result = _repository.AddBrewery(brewery.Id, brewery);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("api/EditBrewery")]
+        public IActionResult EditBrewery([FromBody] Brewery brewery)
+        {
+            if (string.IsNullOrEmpty(brewery.Id))
+            {
+                return Ok("No Id for brewery.  No action taken.");
+            }
+
+            var result = _repository.EditBrewery(brewery.Id, brewery);
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("api/DeleteBrewery/{id}")]
+        public IActionResult DeleteBrewery(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return Ok("No Id for brewery.  No action taken.");
+            }
+
+            var result = _repository.DeleteBrewery(id);
+
+            return Ok(result);
+        }
+
+     }
+}
